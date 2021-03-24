12a79a1b9cd6c7e4f2f57453f3b792c4831c7b7c
Serafettin Bayram <sbayram2016@gmail.com>
2021-03-24 17:07:39 +0100

    html, js and css files created

---
+++ main.js
@@ -0,0 +1,19 @@
+/** First Slider */
+
+$(".slider-one").not(".slick-initialized").slick({
+  autoplay: true,
+  autoplaySpeed: 14000,
+  dots: true,
+  prevArrow: ".site-slider .slider-btn .prev",
+  nextArrow: ".site-slider .slider-btn .next",
+});
+
+/** Second Slider */
+
+// $('.slider-two').not('.slick-initialized').slick({
+//   prevArrow: '.site-slider-two .prev',
+//   nextArrow: '.site-slider-two .next',
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplaySpeed: 3000,
+// });
