ae53c7261a0ff3e87a3925b86cc362959d8919cc
Serafettin Bayram <sbayram2016@gmail.com>
2021-03-24 20:26:14 +0100

    footer background color and main.js updated

--- main.js
+++ main.js
@@ -7,13 +7,3 @@ $(".slider-one").not(".slick-initialized").slick({
   prevArrow: ".site-slider .slider-btn .prev",
   nextArrow: ".site-slider .slider-btn .next",
 });
-
-/** Second Slider */
-
-// $('.slider-two').not('.slick-initialized').slick({
-//   prevArrow: '.site-slider-two .prev',
-//   nextArrow: '.site-slider-two .next',
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   autoplaySpeed: 3000,
-// });
